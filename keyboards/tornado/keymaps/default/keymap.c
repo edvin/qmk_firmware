39f3bb104cd4aac7889f3d76e328ce4e80f71483
Edvin Syse <es@syse.no>
2017-07-28 10:55:19 +0200

    Ins/Del right of Space

--- keyboards/tornado/keymaps/default/keymap.c
+++ keyboards/tornado/keymaps/default/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC,KC_RBRC, \
         F(0), KC_A, KC_S, KC_D, KC_F, KC_G, KC_ENT, KC_H, KC_J, KC_K, KC_L, KC_SCLN,KC_QUOT,KC_BSLS, \
         KC_LSFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM,KC_DOT, KC_SLSH,KC_RSFT, \
-        KC_LCTL,KC_LGUI,KC_LALT,F(1), KC_SPC, KC_RALT,F(2), F(3), KC_RCTL),
+        KC_LCTL,KC_LGUI,KC_LALT,F(1), KC_SPC, KC_INS,KC_DEL, F(2), KC_RCTL),
     /* 1: Numpad layer */
     KEYMAP_TORNADO(
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_DEL,KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS, KC_TRNS,  \
@@ -29,7 +29,6 @@ const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1), // To Mouse overlay
     [1] = ACTION_LAYER_MOMENTARY(2), // To F + Arrow Keys overlay
     [2] = ACTION_LAYER_TOGGLE(1),    // Toggle F + Arrow Keys overlay,
-    [3] = ACTION_LAYER_TOGGLE(2),    // Toggle Mouse overlay
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
