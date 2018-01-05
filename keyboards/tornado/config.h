c61c47aa29851e0ff60cfb4ce5e954298a6fd5b3
Edvin Syse <es@syse.no>
2018-01-05 14:19:15 +0100

    Added TornadoKeyboard PID

--- keyboards/tornado/config.h
+++ keyboards/tornado/config.h
@@ -22,7 +22,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x6060
+#define PRODUCT_ID   0xEF5E
 #define DEVICE_VER   0x0001
 #define MANUFACTURER    Tornado
 #define PRODUCT         TornadoKeyboard
